8ab91e717a018205fe44460552c04fb670dd7515
azeembrand12 <azeempk798@gmail.com>
2025-02-09 11:13:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+gsap.to("#box1",{
+    y:400,
+    duration:3,
+    delay:1,
+    rotate:540,
+    yoyo:true,
+    repeat:1,
+});
+gsap.to("#box2",{
+    x:200,
+    duration:3,
+    delay:1,
+    rotate:540,
+    repeat:1,
+    yoyo:true,
+ 
+});
+gsap.to("#box3",{
+    y:-400,
+    duration:3,repeat:1,
+    delay:1, yoyo:true,
+    rotate:540,
+   
+});
+gsap.to("#box4",{
+    y:-600,
+    x:400,repeat:1,
+    duration:3,
+    delay:1, yoyo:true,
+    rotate:540
+});
+gsap.to("#box5",{
+    y:-400,
+    x:400,repeat:1,
+    duration:3, 
+    delay:1,yoyo:true,
+    rotate:540,
+
+});
+// gsap.from("#box1",{
+//     y:400,
+//     duration:2,
+//     delay:1,
+//     rotate:540,
+
+// });
+// gsap.from("#box2",{
+//     x:200,
+//     duration:2,
+//     delay:2,
+//     rotate:540,
+ 
+// });
+// gsap.from("#box3",{
+//     y:-400,
+//     duration:2,
+//     delay:3, 
+//     rotate:540
+// });
+// gsap.from("#box4",{
+//     y:-600,
+//     x:400,
+//     duration:2,
+//     delay:4, 
+//     rotate:540
+// });
+// gsap.from("#box5",{
+//     y:-400,
+//     x:400,
+//     duration:2, 
+//     delay:5,
+//     rotate:540
+// });
